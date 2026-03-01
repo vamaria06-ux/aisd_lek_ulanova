6e1c6a14da51f0d57401efdb673c807ce661c23e
vamaria06-ux <ulanovamaria0906@yandex.ru>
2026-03-01 22:11:26 +0300

    описание неявного интерфейса

--- aisdlek1.cpp
+++ aisdlek1.cpp
@@ -1,2 +1,20 @@
+template <class T>
+struct Bilist 
+{
+  T val;
+  Bilist<T>* next;
+  Bilist<T>* prev;
+};
+// неявный интерфейс
+template <class T>
+Bilist<T>* add(Bilist<T>* h, const T& v);
+template <class T>
+Bilist<T>* insert(Bilist<T>* h, const T& v);
+template <class T>
+Bilist<T>* cut(Bilist<T>* h) noexcept;
+template <class T>
+Bilist<T>* erase(Bilist<T>* h) noexcept;
+template <class T>
+Bilist<T>* clear(Bilist<T>* h, Bilist<T>* e) noexcept;
 int main()
 {}
